37f1b05d7d8e383a564326ff8fe70f8fee375ed5
Erick Asto Oblitas <zerazobz@outlook.com>
2015-11-23 22:28:50 -0500

    ->Interception Example Working

---
+++ InterceptionConsole/Interceptors/LoggingInterceptor.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.Practices.Unity.InterceptionExtension
+
+Public Class LoggingInterceptor
+    Implements IInterceptionBehavior
+
+    Public ReadOnly Property WillExecute As Boolean Implements IInterceptionBehavior.WillExecute
+        Get
+            Return True
+        End Get
+    End Property
+
+    Public Function GetRequiredInterfaces() As IEnumerable(Of Type) Implements IInterceptionBehavior.GetRequiredInterfaces
+        Return Enumerable.Empty(Of Type)
+    End Function
+
+    Public Function Invoke(input As IMethodInvocation, getNext As GetNextInterceptionBehaviorDelegate) As IMethodReturn Implements IInterceptionBehavior.Invoke
+        Dim result As IMethodReturn = getNext()(input, getNext)
+        Console.WriteLine("Intercepted")
+        Return result
+    End Function
+End Class
--- InterceptionConsole/Module1.vb
+++ InterceptionConsole/Module1.vb
@@ -1,7 +1,14 @@
-﻿Module Module1
+﻿Imports Microsoft.Practices.Unity
+Imports Microsoft.Practices.Unity.InterceptionExtension
+
+Module Module1
 
     Sub Main()
-        Dim someTask As ITask = New ATask()
+        Dim myContainer As IUnityContainer = New UnityContainer()
+        myContainer.AddNewExtension(Of Interception)()
+        myContainer.RegisterType(Of ITask, ATask)(New Interceptor(Of InterfaceInterceptor), New InterceptionBehavior(Of LoggingInterceptor))
+
+        Dim someTask As ITask = myContainer.Resolve(Of ITask)
         someTask.DoSomething()
 
         Console.ReadKey()
